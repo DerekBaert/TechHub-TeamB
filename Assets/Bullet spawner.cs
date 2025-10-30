cc069e158cad8d26078c4941a06db11d51bb4a67
DUCKFIREEE <amicic24@7oaks.org>
2025-10-30 14:42:53 -0500

    BULLET STUFF
    YOULL NEED THIS WE NEED THIS

---
+++ Assets/Bullet spawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Bulletspawner : MonoBehaviour
+{
+   
+    
+        public float bulletlife = 1;
+        public float rocation = 0; 
+        public float speed = 1;
+}
+void Start()
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
