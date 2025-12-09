e830d11ff65129ec2d41b0c8d4b2254cdfc1fcb5
DUCKFIREEE <amicic24@7oaks.org>
2025-12-09 12:01:20 -0600

    Adjusting bullet spawner

--- Assets/Bullet spawner.cs
+++ Assets/Bullet spawner.cs
@@ -6,51 +6,52 @@ using UnityEngine;
 public class BulletSpawner : MonoBehaviour
 {
     enum SpawnerType { Straight, Spin, Backtfourth }
+
     [Header("Bullet Attributes")]
     public GameObject bullet;
-    public float bulletLife = 1f; 
+    public float bulletLife = 1f;
     public float speed = 1f;
-
-  
-
-    Vector3 nextpos;
+    public Transform pos1, pos2; // This Is for the BackTForth Spawner
+    public Transform StartPos;
 
     [Header("Spawner Attributes")]
     [SerializeField] private SpawnerType spawnerType;
     [SerializeField] private float FiringRate = 1f;
+    [SerializeField] private float startDelay = 10f; // Added delay before spawning starts
 
+    Vector3 nextpos;
     public GameObject spawnedBullet;
-    private float timer = 0f; // Something
+
+    private float timer = 0f;
+    private float delayTimer = 0f; // Timer for tracking the delay
 
     void Start()
     {
-         
+        delayTimer = startDelay;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime; 
-        if (spawnerType == SpawnerType.Spin) transform.eulerAngles = new Vector3 (0f, 0f, transform.eulerAngles.z+1f);
-        if (timer >= FiringRate)
+        // Handle the initial delay
+        if (delayTimer > 0)
         {
-            timer = 0f;
-            Fire();
-
+            delayTimer -= Time.deltaTime;
+            return; // Don't process firing logic while delaying
         }
-        
-        
+
+        if (spawnerType == SpawnerType.Spin)
+            transform.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z + 1f);
+
+        timer += Time.deltaTime;
         if (timer >= FiringRate)
-          
-       
         {
             timer = 0f;
             Fire();
-
         }
     }
-    private void Fire() {
 
+    private void Fire()
+    {
         if (bullet)
         {
             spawnedBullet = Instantiate(bullet, transform.position, Quaternion.identity);
@@ -59,4 +60,4 @@ public class BulletSpawner : MonoBehaviour
             spawnedBullet.transform.rotation = transform.rotation;
         }
     }
-    }
+}
