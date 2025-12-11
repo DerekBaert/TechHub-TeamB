f0ea10ce2520801dbd736c6e55e55ce80d398f8f
CTGF64 <topazzthe1st@gmail.com>
2025-12-11 13:37:25 -0600

    added timer to fish flake spawner

--- Assets/Random Spawner.cs
+++ Assets/Random Spawner.cs
@@ -1,20 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class RandomSpawner : MonoBehaviour
-{ 
+{
 
     public GameObject itemPrefab;
     public float Radius = 1;
- 
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) SpawnObjectAtRadom();
+        if (Input.GetKeyDown(KeyCode.E))
+            StartCoroutine(SpawnFlakes());
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.paleGreen;
+
+        Gizmos.DrawWireSphere(this.transform.position, Radius);
+    }
+    IEnumerator SpawnFlakes()
+    {
+        yield return new WaitForSeconds(5f);
+        SpawnObjectAtRadom();
         {
         }
         void SpawnObjectAtRadom() { }
-            Vector3 randomPos = Random.insideUnitCircle * Radius;
+        Vector3 randomPos = Random.insideUnitCircle * Radius;
 
 
         Instantiate(itemPrefab, randomPos, Quaternion.identity);
@@ -23,12 +36,5 @@ public class RandomSpawner : MonoBehaviour
         }
         ;
     }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.paleGreen;
-
-        Gizmos.DrawWireSphere(this.transform.position, Radius);
-    }
-
 }
 
